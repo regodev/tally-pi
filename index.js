034eb9886da9edd0b1d1d5f703cfcfc3f28d2ad5
Peter <that.peter@gmail.com>
2022-05-17 12:20:45 +0200

    invert

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function init() {
   gpio.setMode(gpio.MODE_BCM);
   for (let i = 0; i < mapping.length; i++) {
     console.log('setup', mapping[i]);
-    const res = await gpiop.setup(mapping[i], gpio.DIR_OUT);
+    const res = await gpiop.setup(mapping[i], gpio.DIR_HIGH);
     console.log('done');
   }
 
@@ -28,13 +28,17 @@ async function init() {
 async function loop() {
   for (let i = 0; i < mapping.length; i++) {
     console.log('setting gpo pin', mapping[i]);
-    await gpiop.write(mapping[i], i === loopIdx);
+    await setGPO(i, i !== loopIdx);
   }
 
   loopIdx++;
   if (loopIdx >= mapping.length) {
     loopIdx = 0;
   }
+}
+
+async function setGPO(idx, value) {
+  return gpiop.write(mapping[idx], !value);
 }
 
 console.log('init..');
