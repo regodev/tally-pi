ac14e9183613c51f010836524fe5f7b893210b86
Peter <that.peter@gmail.com>
2022-05-17 12:01:20 +0200

    initial

---
+++ index.js
@@ -0,0 +1,39 @@
+const gpio = require('rpi-gpio').promise;
+
+let loopIdx = 0;
+
+const mapping = [
+  5,
+  6,
+  13,
+  16,
+  19,
+  20,
+  21,
+  26
+];
+
+async function init() {
+  for (let i = 0; i < mapping.length; i++) {
+    const res = await gpio.setup(mapping[i], gpio.DIR_OUT);
+  }
+
+  setInterval(() => loop(), 1000);
+}
+
+async function loop() {
+  for (let i = 0; i < mapping.length; i++) {
+    console.log('setting gpo pin', mapping[i]);
+    await gpio.write(mapping[i], i === loopIdx);
+  }
+
+  loopIdx++;
+  if (loopIdx >= mapping.length) {
+    loopIdx = 0;
+  }
+}
+
+console.log('init..');
+init()
+  .then(() => console.log('ok'))
+  .catch(err => console.error(err));
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Tally GPO Raspberry Pi
+
+## Install
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+sudo apt-get install -y nodejs
+sudo npm install -g pm2
+
+## Start
+pm2 start index.js
+pm2 startup systemd
+pm2 save
