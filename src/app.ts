3b847b2b48735b472546ac0f1eac2f925d33947d
Peter <that.peter@gmail.com>
2025-04-08 19:21:00 +0200

    Remove Numato in favour of cro events to gpio-guy (#2)
    * Remove Numato dependency
    
    * v 1.0.3 remove numato
    
    * 1.0.2

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ let mapping = [
 ];
 
 if (process.platform === 'win32') {
-  gpiolib = require('./numato');
+  gpiolib = require('./gpioguy');
   mapping = [0, 1, 2, 3, 4, 5, 6, 7];
 } else {
   gpiolib = require('rpi-gpio');
@@ -122,7 +122,7 @@ async function updateTallies(arr: any[]) {
     if (mappingIdx != null) {
       if (arr[i].tally) {
         try {
-          await setGPO(mappingIdx, !!(arr[i].tally.tally1 || arr[i].tally.tally2));
+          await setGPO(mappingIdx, !!((arr[i].tally.tally1 > 0) || (arr[i].tally.tally2 > 0)));
         } catch (err) {
           error('Failed to update tally', mappingIdx, err);
         }
@@ -157,7 +157,7 @@ async function setGPO(idx: number, value: boolean) {
   if (debug) {
     log(`Setting gpo pin ${mapping[idx]} to ${value}`);
   }
-  return gpiop.write(mapping[idx], !value);
+  return gpiop.write(mapping[idx], value);
 }
 
 log(`Init.. hostname: ${HOSTNAME}`);
